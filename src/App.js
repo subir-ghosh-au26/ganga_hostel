7bf9e8d427cedab97479b8de6794d3c2eebe731e
Subir Ghosh <subirghghosh@gmail.com>
2024-10-05 18:41:55 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
-
+import Ground from './Ground';
+import React from 'react';
+import Second from './Second';
+import First from './First';
+import Third from './Third';
 function App() {
   return (
     <div className="App">
+      <div className='Full'>
+      <img src={logo} className="App-logo" alt="logo" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className="title">BIPARD Ganga Hostel</h2> 
       </header>
+      <div className='Floor'>
+      <Ground />
+      <First />
+      <Second />
+      <Third />
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/Second.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./Modal.css";
+import second from "./pic/second.png"
+
+export default function Second() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if(modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+  return (
+    <children>
+      <div className="DataView">
+      <img src={second} alt="2nd" />
+      <button onClick={toggleModal} className="btn-modal">
+        Second Floor
+      </button>
+      </div>
+
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+          <p>Ground Floor</p>
+            <table>
+              <tr>
+                <th>Total Room</th>
+                <th>Suite Room</th>
+                <th>Single Room</th>
+              </tr>
+              <tr>
+                <td>24</td>
+                <td>08</td>
+                <td>08</td>
+              </tr>
+            </table>
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+    </children>
+  );
+}
